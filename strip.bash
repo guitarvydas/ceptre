c10697fb4cc627596201ebd28a941434385987fe
Paul Tarvydas <guitarvydas@gmail.com>
2023-03-06 09:52:27 -0500

    rm _ in strings step 5

--- strip.bash
+++ strip.bash
@@ -2,4 +2,5 @@
 sed -E 's/ +,/,/g' <$1 >/tmp/120
 sed -E 's/ +\)/)/g' </tmp/120 >/tmp/130
 sed -E 's/ +\(/(/g' </tmp/130 >/tmp/140
-sed -E 's/ +/ /g' </tmp/140 >$2
+sed -E 's/ +/ /g' </tmp/140 >/tmp/150
+./vstrip </tmp/150 >$2
