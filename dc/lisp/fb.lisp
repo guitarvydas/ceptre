110f6f89ab01f0cb8c4f19e7948fd4fd8a9c5edb
Paul Tarvydas <guitarvydas@gmail.com>
2023-03-08 07:05:09 -0500

    bind to lvar test - remove debugs

--- dc/lisp/fb.lisp
+++ dc/lisp/fb.lisp
@@ -6,20 +6,17 @@
 (defclass lvar ()
   ((hole :accessor hole :initform nil)))
 
-(defun bind (self v)
-  (let ((is-lvar (eq 'lvar (type-of self))))
-    (cond ((not is-lvar)
-           (format *error-output* "bindA ~a ~a~%" self v)
-           (cond ((equal self v) (values v t))
-                 ((not (equal self v))
-                  (values nil nil))))
-          (is-lvar
-           (format *error-output* "bindB ~a ~a~%" self v)
-           (cond ((null (hole self)) (setf (hole self) v) (values v t))
-                 ((not (null (hole self)))
-                  (cond ((equal v (hole self)) (values v t))
-                        ((not (equal v (hole self)))
-                         (values nil nil)))))))))
+(defmethod bind ((self lvar) v)
+  (cond ((null (hole self)) (setf (hole self) v) (values v t))
+        ((not (null (hole self)))
+         (cond ((equal v (hole self)) (values v t))
+               ((not (equal v (hole self)))
+                (values nil nil))))))
+
+(defmethod bind ((self T) v)
+  (cond ((equal self v) (values v t))
+        ((not (equal self v))
+         (values nil nil))))
 
 (defmethod clear ((self lvar))
   (setf (hole self) nil))
@@ -37,12 +34,9 @@
 
 
 (defun match-one (predicate-from-fb predicate) 
-(format *error-output* "match-one ~a ~a~%" predicate-from-fb predicate)
   (mapc #'(lambda (arg actual)
-(format *error-output* "match-one-mapc ~a ~a~%" arg actual)
             (multiple-value-bind (v success) (bind arg actual)
               (declare (ignore v))
-(format *error-output* "match-one-bind ~a~%" success)
               (cond
                (success nil)
                ((not success) (return-from match-one nil)))))
