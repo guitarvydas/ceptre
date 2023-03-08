9ea4b160c4bf5e81a1440656cc9a402177b87eb2
Paul Tarvydas <guitarvydas@gmail.com>
2023-03-08 08:00:13 -0500

    first light lisp stages "first" and "second"

---
+++ dc/lisp/m.lisp
@@ -0,0 +1,56 @@
+;;; stage first {
+;;;   one : x -o y.
+;;; }
+
+;;; stage second {
+;;;   two : y -o z.
+;;; }
+
+;;; qui * stage first -o stage second.
+;;; qui * stage second -o quit.
+
+(defun stage-first ()
+  (cond ((and (match `(x)))
+         (retract `(x))
+         (assert `(y)))
+        (t (assert `(qui)))))
+
+(defun stage-second ()
+  (cond ((and (match `(y)))
+         (retract `(y))
+         (assert `(z)))
+        (t (assert `(qui)))))
+              
+(defun run-stage ()
+  (cond ((match `(stage "first"))
+         (stage-first))
+        ((match `(stage "second"))
+         (stage-second))
+        (t (error "internal error in run-stage"))))
+
+(defun top-level ()
+  (format *error-output* "top-level: ~a~%" *fb*)
+  (cond ((and (match `(qui))
+              (match `(stage "first")))
+         (retract `(qui))
+         (retract `(stage "first"))
+         (assert `(stage "second")))
+        ((and (match `(qui))
+              (match `(stage "second")))
+         (retract `(qui)) 
+         (retract `(stage "second"))
+         (assert `(quit)))
+        ((and (match `(quit)))
+         (retract `(quit))
+         (return-from top-level))
+        (t
+         (run-stage)))
+  (top-level))
+
+(defun run ()
+  (clear-fb)
+  (clear-lvars)
+  (assert `(stage "first"))
+  (top-level)
+  (values))
+
